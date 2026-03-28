6140ef255e8bc9fddf5c2bc80df1ae9e59f14945
Rojemy <roje.diaz@gmail.com>
2026-03-27 23:36:23 -0600

    Created function to display menu

---
+++ 03-python-basico/11-proyecto-final/menu.py
@@ -0,0 +1,11 @@
+def display_menu():
+    while True:
+
+        print("\n*** Sistema Control de Estudiantes ***")
+        print("1. Ingresar nuevo estudiante")
+        print("2. Listar todos los estudiantes")
+        print("3. Top 3 - Mejores promedios")
+        print("4. Ver nota promedio entre las notas de todos los estudiantes")
+        print("5. Exportar datos a CSV")
+        print("6. Importar datos de un CSV (previamente exportado)")
+        print("7. Salir")
